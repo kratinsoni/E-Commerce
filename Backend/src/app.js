b9f4639f998a02f58506ac0932c176166af456e0
Harshit Soni <kratin67soni@gmail.com>
2024-05-19 01:29:13 +0530

    chore: Add static file serving for public folder

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -14,6 +14,7 @@ app.use(
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
+app.use(express.static("public")); //serving static files
 
 //routes import
 
