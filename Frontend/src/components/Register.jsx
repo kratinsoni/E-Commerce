89032d37a7208a6b340f2e9d422d78878390d792
Harshit Soni <kratin67soni@gmail.com>
2024-05-19 01:18:50 +0530

    chore: Update input id in Register component to "avatar"

--- Frontend/src/components/Register.jsx
+++ Frontend/src/components/Register.jsx
@@ -97,7 +97,7 @@ const Register = () => {
           </label>
           <div class="grid w-full max-w-xs items-center gap-1.5">
             <input
-              id="file"
+              id="avatar"
               type="file"
               class="flex h-10 w-full rounded-md border border-input bg-white px-3 py-2 text-sm text-gray-400 file:border-0 file:bg-transparent file:text-gray-600 file:text-sm file:font-medium cursor-pointer"
               onChange={(e) => setAvatar(e.target.files[0])}
