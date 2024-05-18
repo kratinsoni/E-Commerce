0b151350fc35184ee9b65c3f24ab32d85606ce8e
Harshit Soni <kratin67soni@gmail.com>
2024-05-18 16:22:35 +0530

    chore: Update proxy target URL and add options in vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -7,6 +7,8 @@ export default defineConfig({
     proxy: {
       "/api": {
         target: "https://e-commerce-w4cf.onrender.com",
+        changeOrigin: true,
+        secure: false,
       },
     },
   },
